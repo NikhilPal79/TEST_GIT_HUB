54346b279b024525cabc42161ac39d1231f302c5
NikhilPal79 <nikhilchpr7@gmail.com>
2025-03-18 11:10:22 -0400

    modifing 1st time

---
+++ src/FIRST.java
@@ -0,0 +1,15 @@
+public class FIRST {
+
+    /*A private member cannot be accessed directly from outside the class.
+    The aim is to keep it hidden from the users and other classes.
+    It is a popular practice to keep the data members private since we do not want
+    anyone manipulating our data directly. We can make members private using the keyword private.
+
+
+    A private member cannot be accessed directly from outside the class.
+    The aim is to keep it hidden from the users and other classes.
+    It is a popular practice to keep the data members private since we do not want
+    anyone manipulating our data directly. We can make members private using the keyword private.
+*/
+
+}
