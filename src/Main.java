2f1ded042286a62d6367e0ab6d77513a8286b201
NikhilPal79 <nikhilchpr7@gmail.com>
2025-03-18 11:02:59 -0400

    copy paste

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,13 @@ public class Main {
         A private member cannot be accessed directly from outside the class.
         The aim is to keep it hidden from the users and other classes.
                 It is a popular practice to keep the data members private since we do not want anyone manipulating our data directly.
+                We can make members private using the keyword private.
+
+                A private member cannot be accessed directly from outside the class.
+        The aim is to keep it hidden from the users and other classes.
+                It is a popular practice to keep the data members private since we do not want anyone manipulating our data directly.
                 We can make members private using the keyword private.*/
+
     }
 
 
