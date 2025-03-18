8ab7f8c27590bcd3376a82591abf596a060ec2a4
NikhilPal79 <nikhilchpr7@gmail.com>
2025-03-18 12:56:20 -0400

    add in  main

---
+++ src/mar18.java
@@ -0,0 +1,8 @@
+public class mar18 {
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
