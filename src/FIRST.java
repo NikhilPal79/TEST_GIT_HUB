d0c4b4c044c481069b07d4abe502ae99c34da31a
NikhilPal79 <nikhilchpr7@gmail.com>
2025-03-18 11:15:50 -0400

    add sout

--- src/FIRST.java
+++ src/FIRST.java
@@ -1,4 +1,7 @@
 public class FIRST {
+    public static void main(String[] args) {
+
+
 
     /*A private member cannot be accessed directly from outside the class.
     The aim is to keep it hidden from the users and other classes.
@@ -11,5 +14,7 @@ public class FIRST {
     It is a popular practice to keep the data members private since we do not want
     anyone manipulating our data directly. We can make members private using the keyword private.
 */
+        System.out.println("hello i ma learing git hub pull request");
+    }
 
 }
